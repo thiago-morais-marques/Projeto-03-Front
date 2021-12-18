b86c3407e8dcdc6dc5b1256fe80dc041fb2b9c94
THGMMX <morais.marques@hotmail.com>
2021-12-17 21:47:58 -0300

    updated

--- src/components/Templates/Posts/PostCreate/PostCreate.jsx
+++ src/components/Templates/Posts/PostCreate/PostCreate.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import * as yup from 'yup';
 import { useFormik } from 'formik';
-import { Editor } from 'react-draft-wysiwyg';
-import { EditorState } from 'draft-js';
+import { Editor, EditorState } from 'draft-js';
 import { styled } from '@mui/material/styles';
 import {
-  Container, Button, Grid, Card, MuiSnackBar,
+  Container, Button, Grid, Card, TextareaAutosize,
 } from '@mui/material';
 import { Form, Col } from 'react-bootstrap';
+import { createOnePost } from '../../../../service/api';
 import HeroImage from '../../../Misc/HeroImage';
 import TemplatePage from '../../TemplatePage';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import '../../../../assets/styles/PostCreate.css';
+import MuiSnackBar from '../../../Misc/Snackbar';
 
 const Input = styled('input')({
   display: 'none',
@@ -32,10 +31,6 @@ const PostCreate = () => {
   const [disabled, setDisabled] = useState(false);
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty());
 
-  useEffect(() => {
-    console.log(editorState);
-  }, [editorState]);
-
   const handleReaderLoaded = (e) => {
     const binaryString = e.target.result;
     const toBase64 = window.btoa(binaryString);
@@ -44,41 +39,34 @@ const PostCreate = () => {
     setDisabled(true);
   };
 
+  const token = localStorage.getItem('token');
+
   const {
     values, touched, errors,
-    handleChange, handleBlur, handleSubmit,
-    setValues, setErrors,
+    handleChange, handleBlur, handleSubmit, setErrors,
   } = useFormik({
     initialValues: {
-      userName: '',
-      name: '',
-      email: '',
-      password: '',
-      passwordConfirmation: '',
+      title: '',
+      description: '',
+      text: '',
+      imageURL: '',
     },
     validationSchema: loginSchema,
     onSubmit: async (formData) => {
       try {
-        await editUser({
-          name: formData.name,
-          userName: formData.userName,
-          email: formData.email,
-          password: formData.password,
-          profilePicture: attach,
+        await createOnePost({
+          title: formData.title,
+          description: formData.description,
+          text: formData.text,
+          imageURL: attach,
         }, token);
-        localStorage.removeItem('token');
-        const tokenResponse = await login(formData.email, formData.password);
-        localStorage.setItem('token', tokenResponse.token);
-        handleClose();
-        document.location.reload(true);
       } catch (error) {
         setErrors([{
-          email: error.response.data.error,
+          title: error.response.data.error,
         }]);
       }
     },
   });
-
 
   const handleChangeFile = (e) => {
     const file = e.target.files[0];
@@ -90,63 +78,85 @@ const PostCreate = () => {
     }
   };
 
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
-
   return (
     <div>
       <HeroImage />
       <Container maxWidth="lg" sx={{ height: '100%' }}>
         <TemplatePage>
-          <Grid sx={{ alignItems: 'center', height: '100%' }}>
-            <Card sx={{ display: 'flex', flexDirection: 'column', p: 5, my: 4 }}>
-              <Grid style={{ display: 'flex', alignContent: 'center' }}>
-                <Form.Group className="title-container" as={Col} md="12" controlId="login-form">
-                  <Form.Label>
-                    <h2>Título:</h2>
-                  </Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={value}
-                    onChange={handleChange}
-                  />
-                </Form.Group>
-              </Grid>
-              <h5>Sobre o que você gostaria de falar hoje?</h5>
-              <div
-                style={{
-                  marginBottom: '1rem',
-                  border: '1px solid black',
-                  padding: '2px',
-                  minHeight: '400px',
-                  marginTop: '20px',
+          <form onSubmit={handleSubmit}>
+            <Grid sx={{ alignItems: 'center', height: '100%' }}>
+              <Card
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  p: 5,
+                  my: 4,
                 }}
               >
-                <Editor
-                  editorState={editorState}
-                  onEditorStateChange={setEditorState}
-                />
-              </div>
-              <Grid className="button-container" sx={{ justifyContent: 'space-between' }}>
-                <label htmlFor="contained-button-file">
-                  <Input accept="image/*" id="contained-button-file" multiple type="file" />
-                  <Button variant="contained" component="span" disabled={disabled}>
-                    Upload
-                  </Button>
-                  <MuiSnackBar type="success" msg="Foto anexada com sucesso!" open={open} setOpen={setOpen} />
-                </label>
-                <Button
-                  className="submit"
-                  type="submit"
-                  fullWidth
-                  variant="contained"
+                <Grid
+                  style={{
+                    display: 'flex',
+                    pb: 4,
+                    flexDirection: 'column',
+                    alignContent: 'center',
+                  }}
                 >
-                  Publicar
-                </Button>
-              </Grid>
-            </Card>
-          </Grid>
+                  <Form.Group as={Col} md="12">
+                    <Form.Label>
+                      <h2>Título:</h2>
+                    </Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={values.title}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      isValid={touched.title && !errors.title}
+                      isInvalid={touched.title && errors.title}
+                    />
+                  </Form.Group>
+                  <Grid sx={{ py: 4 }}>
+                    <h4>Descrição:</h4>
+                    <TextareaAutosize
+                      maxRows={3}
+                      placeholder="Máximo de 3 linhas"
+                      style={{ width: '100%', height: '5rem', p: 4 }}
+                    />
+                  </Grid>
+                </Grid>
+                <h5>Sobre o que você gostaria de falar hoje?</h5>
+                <div
+                  style={{
+                    marginBottom: '1rem',
+                    border: '1px solid black',
+                    padding: '2px',
+                    minHeight: '400px',
+                    marginTop: '20px',
+                  }}
+                >
+                  <Editor
+                    editorState={editorState}
+                    onChange={setEditorState}
+                  />
+                </div>
+                <Grid sx={{ display: 'flex', justifyContent: 'space-between', mx: 2 }}>
+                  <label htmlFor="contained-button-file">
+                    <Input accept="image/*" id="contained-button-file" multiple type="file" />
+                    <Button variant="contained" component="span" disabled={disabled} onChange={handleChangeFile}>
+                      Upload
+                    </Button>
+                    <MuiSnackBar type="success" msg="Foto anexada com sucesso!" open={open} setOpen={setOpen} />
+                  </label>
+                  <Button
+                    className="submit"
+                    type="submit"
+                    variant="contained"
+                  >
+                    Publicar
+                  </Button>
+                </Grid>
+              </Card>
+            </Grid>
+          </form>
         </TemplatePage>
       </Container>
     </div>
